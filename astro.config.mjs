55f11d4ce82cf13fe9e87a817f14f1236c590316
Mhd Rizaldy Andrian Rambe <mhd.rambe@Mhds-MacBook-Pro.local>
2025-12-31 13:08:29 +0700

    Setup GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aldyrambe.dev',
+  site: "https://aldyrmbe.github.io",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
-      wrap: true
-    }
-  }
+      theme: "github-dark",
+      wrap: true,
+    },
+  },
 });
